dc3ab6d474d1b5bc4330c41f00a82ea941798162
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-04-01 03:38:30 +0600

    Added comments and some housekeeping

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -2,26 +2,28 @@
 
 namespace App\Controller;
 
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
 class MyController extends AbstractController {
 
-
+    //function to render index page
     public function index() {
-
         return $this->render('index.html.twig');
     }
 
-
+    //function to upload the image/file and render the index page casually
     public function imageUpload(Request $request){
 
+        //getting the file from the form with key as 'myfile'
         $file = $request->files->get('myfile');
+
+        //selecting the server directory to save the image/file
         $dir = $request->server->get('DOCUMENT_ROOT') . '/assets/files';
 
         $fileName = $file->getClientOriginalName();
+
+        //with move function we are saving the file to the directory
         $file->move($dir, $fileName);
 
         return $this->render('index.html.twig');
